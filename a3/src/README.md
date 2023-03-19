20e3166399ad51f4dc6f98cecc23610f2b3634c8
Amir Mohamad <amohamad@my.yorku.ca>
2023-03-19 00:06:54 +0000

    Add A3 srcML method scripts

--- a3/src/README.md
+++ a3/src/README.md
@@ -7,4 +7,9 @@
    3. run `script/final-containment.sh -j lib/ql.jar -t <subsystem>.raw.ta -c <contain-filename> -n <subsystems-name>` (create final .con.ta, ls.ta, ignored.txt files)
    4. run `python script/clean-ignored.py -t <subsystems-name>.ls.ta -i ignored.txt -r res.txt` (filter ignored files from ls.ta)
    5. run `script/ls-edit.sh -j lib/lseditor-7.3.13.jar -f <subsystems-name>.ls.ta` (visualize file dependency with containment structure)
-3. to-do, srcML
+3. SrcML Include Extraction Scripts
+   1. run `python script/gen-ta-file-srcml.py -t <subsystem>.raw.ta -ss <entire-subsystem> -s <src_path> -x <srcml-xml-file>` (create raw.ta file)
+   2. run `python script/gen-containment.py -t <subsystem>.raw.ta -c <containment-file-name> -ss <subsystems-json-file> -s <src_path>` (create contain file)
+   3. run `script/final-containment.sh -j lib/ql.jar -t <subsystem>.raw.ta -c <contain-filename> -n <subsystems-name>` (create final .con.ta, ls.ta, ignored.txt files)
+   4. run `python script/clean-ignored.py -t <subsystems-name>.ls.ta -i ignored.txt -r res.txt` (filter ignored files from ls.ta)
+   5. run `script/ls-edit.sh -j lib/lseditor-7.3.13.jar -f <subsystems-name>.ls.ta` (visualize file dependency with containment structure)
--- a3/src/script/gen-ta-file-srcml.py
+++ a3/src/script/gen-ta-file-srcml.py
@@ -3,21 +3,24 @@ import re
 import glob
 import json
 import argparse
+from xml.etree import ElementTree
 
 # Parse args
 parser = argparse.ArgumentParser(description='Generate the <containment-file-name>.contain file')
 parser.add_argument('-t', '--ta-file', type=str, required=True, help='The ta file name to generate')
 parser.add_argument('-ss', '--subsystems', type=str, required=True, help='The subsystems file')
 parser.add_argument('-s', '--source', type=str, required=True, help='The path to the freebsd source code (make sure root is "freebsd")')
+parser.add_argument('-x', '--xml', type=str, required=True, help='The the srcML xml file with includes')
 args = parser.parse_args()
 
 subsystems_file = args.subsystems
 ta_file = args.ta_file
 src_path = args.source
-root = 'freebsd'
+srcml_path = args.xml
+root_path = 'freebsd'
 
 # Create a raw.ta file from header includes, for the defined subsystems.
-# Usage: python gen-ta-file.py -t <raw_ta_dependency_filename> -ss <subsystems-json-file> -s <src_path>
+# Usage: python gen-ta-file-srcml.py -t <raw_ta_dependency_filename> -ss <subsystems-json-file> -s <src_path> -x <srcML-xml-file>
 
 # Subsystems json file structure:
 # {
@@ -41,6 +44,8 @@ sep = os.sep
 with open(subsystems_file) as f:
     subsystems = json.load(f)
 
+tree = ElementTree.parse(srcml_path)
+xml_root = tree.getroot()
 
 def file_exists_in_directory(directory, filename):
     """
@@ -52,47 +57,63 @@ def file_exists_in_directory(directory, filename):
     else:
         return None
 
-def find_directories_with_headers(root_directory):
+def find_include_dependencies():
+    """
+    Parse the srcML include tags for each file.
+    Return a dictionary that maps each file to the set of include files it contains.
+    """
+    file_dependencies = {}
+    for unit in xml_root:
+        filename = unit.attrib['filename'].replace('freebsd-src-release-12.4.0', root_path)
+        include_file = unit.find("{http://www.srcML.org/srcML/cpp}file").text
+        include_file = include_file[1:-1]
+        # include_file = os.path.basename(include_file)
+        if filename not in file_dependencies:
+            file_dependencies[filename] = set()
+        file_dependencies[filename].add(include_file)
+    return file_dependencies
+
+def find_directories_with_headers(src):
     """
     Recursively scan root_directory for directories that contain header files.
     Return a dictionary that maps each directory to the set of header files it contains.
     """
     header_dirs = {}
-    for dirpath, dirnames, filenames in os.walk(root_directory):
+    for dirpath, dirnames, filenames in os.walk(src):
         headers = set(f for f in filenames if f.endswith('.h') or f.endswith('.c'))
         if headers:
             header_dirs[dirpath] = headers
     return header_dirs
 
-def parse_file_dependencies(file, header_dirs):
+def parse_file_dependencies(file_clean, include_map, header_dirs):
     """
-    Parses a file and return a set of full paths of files it depends on.
+    Parses a file and return a set of full paths include files it depends on.
     """
     dependencies = set()
-    try:
-        with open(file, encoding="utf-8", errors="ignore") as f:
-            contents = f.read()
-            # Limitation: might not find all include types
-            includes = re.findall(r'^\s*#\s*include\s+[<"].*?([^\/>"]+)[>"]', contents, flags=re.MULTILINE)
-            for include_file in includes:
-                for header_dir, header_files in header_dirs.items():
-                    if include_file in header_files:
-                        # Limitation: This finds the first match, might not be the right header file
-                        full_path = file_exists_in_directory(header_dir, include_file)
-                        if full_path:
-                            dependencies.add(full_path.replace(src_path, root, 1))
-                            break
-    except Exception:
-        pass
+    includes = include_map.get(file_clean, set())
+    # print(f'found {len(includes)} includes for {file_clean}')
+    for include_file in includes:
+        for header_dir, header_files in header_dirs.items():
+            if include_file in header_files:
+                # Limitation: This finds the first match, might not be the right header file
+                full_path = file_exists_in_directory(header_dir, include_file)
+                if full_path:
+                    dependencies.add(full_path.replace(src_path, root_path, 1))
+                    break
+
     return dependencies
+
 
 # All dependencies
 dependencies = {}
 links = []
 instances = set()
-print('Finding header locations...')
+print('Finding header directories...')
 header_dirs = find_directories_with_headers(src_path)
-print(f'Found {len(header_dirs)} header locations.')
+print('Finding include files...')
+include_map = find_include_dependencies()
+
+print(f'Found {len(include_map)} header locations.')
 
 def print_subsystem(d, fn, parent=None):
     for subsystem, value in d.items():
@@ -116,11 +137,11 @@ def print_subsystem(d, fn, parent=None):
 # Add include level dependencies
 def add_file_dependencies(subsystem, file):
     if file.endswith(('.c', '.cpp', '.h')):
-        file_clean = file.replace(src_path, root, 1)
+        file_clean = file.replace(src_path, root_path, 1)
         instances.add(f'$INSTANCE {file_clean} cFile\n')
 
         # From dependencies
-        dependencies[file] = parse_file_dependencies(file, header_dirs)
+        dependencies[file] = parse_file_dependencies(file_clean, include_map, header_dirs)
         
         if dependencies[file]:
             for dep in dependencies[file]:
